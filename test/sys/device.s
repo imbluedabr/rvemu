76348c07f8549571e8c3a067a9b02231066c0327
imbluedabr <baldergroen@gmail.com>
2026-08-20 15:44:10 +0200

    feat(device): add function to display driver info

--- test/sys/device.s
+++ test/sys/device.s
@@ -11,6 +11,7 @@
     struct dev_ops {
         int (*read)(struct device* dev, void* buff, uint32_t count)
         int (*write)(struct device* dev, void* buff, uint32_t count)
+        int (*ioctl)(struct device* dev, int cmd, void* arg)
     }
 
     struct device {
@@ -29,39 +30,79 @@ device_table_end:
 
     # struct driver* driver_table[4]
 driver_table:
-    .space 64
+    .space 16
 
+str_0:
+    .asciz ":\r\n"
+str_1:
+    .asciz "0x%x, 0x%x\r\n"
 .section .text
 .global device_create
 .global device_lookup
 .global register_driver
 .global list_drivers
 .extern kputs
+.extern kprintf
 
     # void list_drivers()
 list_drivers:
     addi sp, sp, -16
     sw s0, 0(sp)
     sw s1, 4(sp)
+    sw ra, 8(sp)
     la s0, driver_table
     mv s1, zero # int i = 0
 1:
     li t0, 4
     beq s1, t0, 3f
     # struct driver* drv = driver_table[i]
-    slli t0, s1, 4
+    slli t0, s1, 2
     add t0, t0, s0
     lw t0, 0(t0)
     beq t0, zero, 2f # if (drv)
-    # kputs(drv->name)
-    lw a0, 12(t0)
-    call kputs
+    # display_driver_info(drv)
+    mv a0, t0
+    call display_driver_info
 2:
     addi s1, s1, 1
     j 1b
 3:
+    lw ra, 8(sp)
     lw s1, 4(sp)
     lw s0, 0(sp)
+    addi sp, sp, 16
+    ret
+
+    # void display_driver_info(struct driver* drv)
+display_driver_info:
+    addi sp, sp, -16
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+    mv s0, a0
+    
+    # kputs(drv->name)
+    lw a0, 12(s0)
+    call kputs
+    la a0, str_0
+    call kputs
+    # struct device* dev = drv->instance_list
+    lw s1, 4(s0)
+    # while (dev)
+1:
+    beq s1, zero, 2f
+    # kprintf("0x%x, 0x%x\r\n", dev, dev->base)
+    la a0, str_1
+    mv a1, s1
+    lw a2, 8(s1)
+    call kprintf
+    # dev = dev->next
+    lw s1, 4(s1)
+    j 1b
+2:
+    lw s1, 8(sp)
+    lw s0, 4(sp)
+    lw ra, 0(sp)
     addi sp, sp, 16
     ret
 
@@ -69,7 +110,7 @@ list_drivers:
 register_driver:
     # driver_table[major] = drv
     la t0, driver_table
-    slli a0, a0, 4
+    slli a0, a0, 2
     add t0, t0, a0
     sw a1, 0(t0)
     ret
@@ -103,10 +144,10 @@ device_create:
 
     # struct driver* drv = driver_table[major]
     la s0, driver_table
-    slli a0, a0, 4
+    slli a0, a0, 2
     add s0, s0, a0
     lw s0, 0(s0)
-    
+
     # struct device* dev = device_alloc()
     call device_alloc
     mv s1, a0
@@ -136,13 +177,14 @@ device_create:
 
     # struct device* device_lookup(int devno)
 device_lookup:
-    # struct driver* drv = driver_table[driver];
+    # struct driver* drv = driver_table[MAJOR(devno)];
     la t0, driver_table
     andi t1, a0, 0xF0
     srli t1, t1, 2
     add t0, t0, t1
     lw t0, 0(t0)
-    andi a0, a0, 0x0F
+    
+    andi a0, a0, 0x0F # devno = MINOR(devno)
 
     # struct device* dev = drv->instance_list
     lw t1, 4(t0)
